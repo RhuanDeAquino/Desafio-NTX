63e63c990e444e0781e450fff2c5c846a699abde
vai na web <vainawebcpx@gmail.com>
2019-11-08 17:47:31 -0300

    evoluindo...

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 function handleSearch () { 
-	document.querySelector(".pesquisa").style.display='flex';
-	document.querySelector(".pesquisa-icon").style.border='0.5px solid #fff';
+	if (document.querySelector(".pesquisa").style.display == 'none'){
+	 	document.querySelector(".pesquisa").style.display = 'flex';
+	 	document.querySelector(".atividades-informacoes").style.border = '0.1px solid #fff';
 
- if (document.querySelector(".pesquisa").style.display=='none'){
- 	document.querySelector(".pesquisa").style.display='flex';
- }
- else {
- 	document.querySelector(".pesquisa").style.display="none";
- 	document.querySelector(".pesquisa-icon").style.border='0.5px solid #fff';
- }
+	}
+	else {
+	 	document.querySelector(".pesquisa").style.display = "none";
+	 	document.querySelector(".atividades-informacoes").style.border="none";
+	}
+}
+
+
+function handleOpenMenu () {
+	if (document.querySelector(".lista-perfil").style.display = 'none') {
+		document.querySelector(".lista-perfil").style.display = 'flex'
+		document.querySelector(".caixa-perfil").style.border = '0 solid white';
+}
+	else {
+		document.querySelector(".lista-perfil").style.display = "flex";
+		document.querySelector(".caixa-perfil").style.border="flex"
+	}
+}
+
+
+
+
+function handleOpenNotificacao () {
+	if (document.querySelector(".lista-notif").style.display = 'none') {
+		document.querySelector(".lista-notif").style.display = 'flex'
+		document.querySelector(".caixa-notif").style.border = '0';
+}
+	else {
+		document.querySelector(".lista-notif").style.display = "flex";
+		document.querySelector(".caixa-notif").style.border="none"
+	}
 }
