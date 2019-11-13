a8ca196bbf3dbe86a67544f532e1db8791cd7f72
vai na web <vainawebcpx@gmail.com>
2019-11-13 17:32:46 -0300

    Ta fluindo..

--- main.js
+++ main.js
@@ -1,41 +1,43 @@
+function mudarCor(event) {
+	if (window.scroll <= 0) {
+		document.querySelector('header').style.background = 'transparent';
+	}
+	else {
+		document.querySelector('header').style.background = '#ffff';
+	}
+}
+
 function handleSearch () { 
 	if (document.querySelector(".pesquisa").style.display == 'none'){
-	 	document.querySelector(".pesquisa").style.display = 'flex';
+	 	document.querySelector(".pesquisa").style.display = 'flex',
 	 	document.querySelector(".atividades-informacoes").style.border = '0.1px solid #fff';
 
 	}
 	else {
-	 	document.querySelector(".pesquisa").style.display = "none";
+	 	document.querySelector(".pesquisa").style.display = "none",
 	 	document.querySelector(".atividades-informacoes").style.border="none";
 	}
 }
-
-
-function handleOpenMenu () {
-	if (document.querySelector(".lista-perfil").style.display = 'none') {
-		document.querySelector(".lista-perfil").style.display = 'flex'
-		document.querySelector(".caixa-perfil").style.border = '0 solid white';
+/////////////////////////////////////menu///////////////////////////////////////////////
+function handleOpenMenu() {
+	if (document.querySelector(".lista-perfil").style.display == 'none') {
+		document.querySelector(".lista-perfil").style.display = 'flex';
 }
 	else {
-		document.querySelector(".lista-perfil").style.display = "flex";
-		document.querySelector(".caixa-perfil").style.border="flex"
+		document.querySelector(".lista-perfil").style.display = "none";
 	}
 }
+//////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
+//////////////////////////////notificacao//////////////////////////////////////////////////
 function handleOpenNotificacao () {
-	if (document.querySelector(".lista-notif").style.display = 'none') {
-		document.querySelector(".lista-notif").style.display = 'flex'
-		document.querySelector(".caixa-notif").style.border = '0';
+	if (document.querySelector(".lista-notif").style.display == 'none') {
+		document.querySelector(".lista-notif").style.display = 'flex';
 }
 	else {
-		document.querySelector(".lista-notif").style.display = "flex";
-		document.querySelector(".caixa-notif").style.border="none"
+		document.querySelector(".lista-notif").style.display = "none";
 	}
 }
-
 function handleTroca() {
 	if(document.querySelector(".caixa-titulos").style.display = 'flex') {
 		document.querySelector(".caixa-titulos").style.display = 'none'
